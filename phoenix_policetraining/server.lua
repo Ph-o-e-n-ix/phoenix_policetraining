d3e2c1f71bdb4cd0ef6f3bf9962b8674220700bb
Rising Phoenix <119653707+Ph-o-e-n-ix@users.noreply.github.com>
2023-09-21 21:11:01 +0200

    Update server.lua
    Added QB Support

--- phoenix_policetraining/server.lua
+++ phoenix_policetraining/server.lua
@@ -1,23 +1,49 @@
-ESX = exports["es_extended"]:getSharedObject()
+if Config.Framework == 'ESX' then
+   if Config.useNewESX then
+      ESX = exports["es_extended"]:getSharedObject()
+   else 
+      ESX = nil
+      TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+   end
+else 
+   QBCore = exports['qb-core']:GetCoreObject()
+end
 
 local inmissionserver = false
 
 RegisterServerEvent("phoenix:getreward")
 AddEventHandler("phoenix:getreward", function()
-   local xPlayer = ESX.GetPlayerFromId(source)
-   if Config.Reward.item.enable then
-      xPlayer.addInventoryItem(Config.Reward.item.itemname, Config.Reward.item.amount)
-   end 
-   if Config.Reward.money.enable then 
-      xPlayer.addAccountMoney(Config.Reward.money.account, Config.Reward.money.amount)
+   if Config.Framework == 'ESX' then
+      local xPlayer = ESX.GetPlayerFromId(source)
+      if Config.Reward.item.enable then
+         xPlayer.addInventoryItem(Config.Reward.item.itemname, Config.Reward.item.amount)
+      end 
+      if Config.Reward.money.enable then 
+         xPlayer.addAccountMoney(Config.Reward.money.account, Config.Reward.money.amount)
+      end
+   else 
+      local xPlayer = QBCore.Functions.GetPlayer(source)
+      if Config.Reward.item.enable then
+         xPlayer.AddItem(Config.Reward.item.itemname, Config.Reward.item.amount)
+      end 
+      if Config.Reward.money.enable then 
+         xPlayer.AddMoney(Config.Reward.money.amount)
+      end
    end
 end)
 
 if Config.UsableItem.enable then
-   ESX.RegisterUsableItem(Config.UsableItem.itemname, function(source)
-      local xPlayer = ESX.GetPlayerFromId(source)
-      TriggerClientEvent("phoenix:startmission", source)
-   end)
+   if Config.Framework == 'ESX' then
+      ESX.RegisterUsableItem(Config.UsableItem.itemname, function(source)
+         local xPlayer = ESX.GetPlayerFromId(source)
+         TriggerClientEvent("phoenix:startmission", source)
+      end)
+   else
+      QBCore.Functions.CreateUseableItem(Config.UsableItem.itemname, function(source)
+         local xPlayer = QBCore.Functions.GetPlayer(source)
+         TriggerClientEvent("phoenix:startmission", source)
+      end) 
+   end
 end
 
 RegisterServerEvent("phoenix:start_mission_s")
@@ -33,12 +59,20 @@ AddEventHandler("phoenix:starttraining", function()
       inmissionserver = false 
    end
 end)
-
-ESX.RegisterServerCallback('inmissionserver', function(source, cb)
-   if not inmissionserver then 
-      cb(true)
-   else 
-      cb(false)
-   end
-end)
-
+if Config.Framework == 'ESX' then
+   ESX.RegisterServerCallback('inmissionserver', function(source, cb)
+      if not inmissionserver then 
+         cb(true)
+      else 
+         cb(false)
+      end
+   end)
+else 
+   QBCore.Functions.CreateCallback('inmissionserver', function(source, cb)
+      if not inmissionserver then 
+         cb(true)
+      else 
+         cb(false)
+      end
+   end)
+end
